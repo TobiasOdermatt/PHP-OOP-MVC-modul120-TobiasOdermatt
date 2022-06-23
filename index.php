74c3821ea48115fe491e071e6db49aebb80282e4
Tobias Odermatt <90388194+TobiasOdermatt@users.noreply.github.com>
2022-06-24 00:55:53 +0200

    Seitenaufbau nach MVC angepasst

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php 
+//Header mit Seiteninformation
 include 'partials/header.php';
+
+//Navigationsleiste
 include 'partials/nav.php';
-?>
 
+//Hier befindet sich der Seiteninhalt
+include 'view/index.inc.php';
 
-<?php
+//Footerzeile
 include 'partials/footer.php';
 ?>

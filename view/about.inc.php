f4e4d5912a1d945da717ef3fb11345f0f3c77b19
Tobias Odermatt <90388194+TobiasOdermatt@users.noreply.github.com>
2022-06-26 20:18:52 +0200

    About Page hinzugefügt

---
+++ view/about.inc.php
@@ -0,0 +1,39 @@
+        <!-- Page Content-->
+        <div class="container px-4 px-lg-5">
+            <!-- Heading Row-->
+            <div class="row gx-4 gx-lg-5 align-items-center my-5">
+                    <h1 class="font-weight-light">Das Bücherantiquariat in Basel</h1><br>
+                    <h3>Kurzinformation:</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+nihil, eveniet aliquid culpa officia aut! Impedit </p>
+<br>
+                    <h4>Wer sind wir?</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+molestiae quas vel sint commodi repudiandae tias architecto voluptate aliquam
+nihil, eveniet aliquid culpa officia aut! Impedit  accusantium nemo autem. Veritatis
+obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+nihil, eveniet aliquid culpa officia aut! Impedit  accusantium nemo autem. Veritatis
+obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+nihil, eveniet aliquid culpa officia aut! Impedit accusantium nemo autem. Veritatis
+obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+nihil, eveniet aliquid culpa officia aut! Impedit</p><br>
+                </div>
+
+        
+<div class="row gx-4 gx-lg-5 align-items-center my-5">
+<div class="col-lg-4"><img class="img-fluid rounded float-end" src="assets/about_picture.jpg" /><br></div><br>
+<div class="col-lg-7">
+<h3>Ein modernes Buchregal, auf Ihrem Computer:</h3>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,orem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+molestiae quas vel sint commodi repudiandae tias architecto voluptate aliquam
+nihil, eveniet aliquid culpa officia aut! Impedit  accusantium nemo autem. Veritatis
+obcaecati tenetur iure eius earu.</p>
+<p>niet alitetur adipisicing elit. Maxime mollitia,orem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+molestiae quas vel sint commodi repudiandae tias architecto voluptate aliquam
+nihil, eveniet aliquid culpa officia aut! Impedit  accusantium nemo autem. Veritatis
+obcaecati tenetur iure eius earu.</p>
+</div></div></div>

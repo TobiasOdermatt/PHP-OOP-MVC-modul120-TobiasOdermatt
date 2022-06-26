d27122c8fc67668f62bbeb6cd8a0f93b1f3a7b18
Tobias Odermatt <90388194+TobiasOdermatt@users.noreply.github.com>
2022-06-26 20:20:55 +0200

    Session system hinzugefügt

---
+++ helpers/session.inc.php
@@ -0,0 +1,11 @@
+
+<?php
+session_start();
+$userisloggedIn = isset($_SESSION["username"]);
+
+function createSession($mail, $username,$role){
+    $_SESSION["mail"] = $mail;
+    $_SESSION["username"] = $username;
+    $_SESSION["role"] = $role;
+}
+?>

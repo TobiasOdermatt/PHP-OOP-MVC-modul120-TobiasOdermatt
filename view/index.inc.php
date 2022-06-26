affdc49a3a53ff849df2d8f632ea1c29a04aa2f0
Tobias Odermatt <90388194+TobiasOdermatt@users.noreply.github.com>
2022-06-26 20:19:43 +0200

    About Page in Index verlinkt, Errormanagement in Index hinzugefügt

--- view/index.inc.php
+++ view/index.inc.php
@@ -1,12 +1,16 @@
-        <!-- Page Content-->
-        <div class="container px-4 px-lg-5">
+<!-- Page Content-->
+    <div class="container px-4 px-lg-5">
+        <br>
+    <?php include 'helpers/errormanagement.inc.php'; ?>
             <!-- Heading Row-->
             <div class="row gx-4 gx-lg-5 align-items-center my-5">
                 <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="assets/startup_picture.jpg" alt="..." /></div>
                 <div class="col-lg-5">
                     <h1 class="font-weight-light">Wilkommen in dem Bücherantiquariat</h1>
                     <p>Auf dieser Seite können bequem die Kunden- und Bücherdaten sicher verwaltet werden.</p>
-                    <a class="btn btn-primary" href="/about">Mehr informationen</a>
+                    <a class="btn btn-primary" href="?view=about">Mehr informationen</a>
                 </div>
             </div>
-        </div>
+            </div>
+            <br>
+        <br>
